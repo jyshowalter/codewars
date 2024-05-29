cc0b25399890c06d770945bbaa486380426f46de
Jason Showalter <jyshowalter@gmail.com>
2024-05-29 11:52:29 -0700

    added solution

--- highestandlowest.js
+++ highestandlowest.js
@@ -8,3 +8,8 @@
 // All numbers are valid Int32, no need to validate them.
 // There will always be at least one number in the input string.
 // Output string must be two numbers separated by a single space, and highest number is first.
+function highAndLow(numbers){
+  let numArr = numbers.split(" ")
+  let sorted = numArr.sort((a,b) => a - b)
+  return `${sorted[numArr.length-1]} ${sorted[0]}`
+}
