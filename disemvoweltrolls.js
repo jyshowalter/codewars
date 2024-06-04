d345a27a1a57dcd50063d3620ea884d0f838098d
Jason Showalter <jyshowalter@gmail.com>
2024-06-03 22:31:42 -0700

    added solution

--- disemvoweltrolls.js
+++ disemvoweltrolls.js
@@ -7,3 +7,26 @@
 // For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 
 // Note: for this kata y isn't considered a vowel.
+function disemvowel(str) {
+  let newArr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      !(
+        str[i] == "a" ||
+        str[i] == "e" ||
+        str[i] == "i" ||
+        str[i] == "o" ||
+        str[i] == "u" ||
+        str[i] == "A" ||
+        str[i] == "E" ||
+        str[i] == "I" ||
+        str[i] == "O" ||
+        str[i] == "U"
+      )
+    ) {
+      newArr.push(str[i]);
+    }
+  }
+  return newArr.join("");
+}
